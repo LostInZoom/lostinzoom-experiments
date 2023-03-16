d8e66b2f5123b06a2227508dfdb2ec7a0080ef27
jberli <justinberli@gmail.com>
2023-03-16 15:13:45 +0100

    less restrictive configuration

--- anchorwhat/setup.py
+++ anchorwhat/setup.py
@@ -1,13 +1,16 @@
 from anchorwhat.api.initialization import initialize_application
 from anchorwhat.api.configuration import retrieve_configuration
 
-# 3 modes possibles :
+# 2 modes possibles :
 # 'm': Migration mode, no operations made on database
 # 'd': Deployment mode, retrieve configuration from database
 APPLICATION_MODE = 'd'
 
 if APPLICATION_MODE == 'm':
-    print('Anchorwhat migration mode.')
-else:   
-    # App configuration is stored
-    APP_CONFIGURATION = retrieve_configuration('anchorwhat')
+        print('Anchorwhat migration mode.')
+else:
+    try:
+        # App configuration is stored
+        APP_CONFIGURATION = retrieve_configuration('anchorwhat')
+    except:
+        pass
--- fogdetector/setup.py
+++ fogdetector/setup.py
@@ -1,13 +1,16 @@
 from fogdetector.api.initialization import initialize_application
 from fogdetector.api.configuration import retrieve_configuration
 
-# 3 modes possibles :
+# 2 modes possibles :
 # 'm': Migration mode, no operations made on database
 # 'd': Deployment mode, retrieve configuration from database
 APPLICATION_MODE = 'd'
 
 if APPLICATION_MODE == 'm':
-    print('Fog Detector migration mode.')
+        print('Fog Detector migration mode.')
 else:
-    # App configuration is stored
-    APP_CONFIGURATION = retrieve_configuration('fogdetector')
+    try:
+        # App configuration is stored
+        APP_CONFIGURATION = retrieve_configuration('fogdetector')
+    except:
+        pass
